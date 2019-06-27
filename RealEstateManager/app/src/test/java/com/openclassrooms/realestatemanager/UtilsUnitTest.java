a59d275fe91808882f49a87f14b2308aca185bac
GalouM <galou.minisini@gmail.com>
2019-06-27 14:51:33 -0700

    design test working

--- RealEstateManager/app/src/test/java/com/openclassrooms/realestatemanager/UtilsUnitTest.java
+++ RealEstateManager/app/src/test/java/com/openclassrooms/realestatemanager/UtilsUnitTest.java
@@ -1,17 +1,34 @@
 package com.openclassrooms.realestatemanager;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.shadows.ShadowNetworkInfo;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.robolectric.Shadows.shadowOf;
 
 /**
  * Created by galou on 2019-06-21
  */
+@RunWith(AndroidJUnit4.class)
 public class UtilsUnitTest {
+
+    private ConnectivityManager connectivityManager;
+    private ShadowNetworkInfo shadowOfActiveNetworkInfo;
+    private Context context;
 
     @Test
     public void checkConvertDollarToEuro_correct(){
@@ -31,5 +48,25 @@ public class UtilsUnitTest {
         String todayDate = dateFormat.format(new Date());
 
         assertEquals(todayDate, Utils.getTodayDate());
+    }
+
+    @Test
+    public void checkIsInternetAvailableFalse_NoInternet() throws Exception{
+        this.setUpContextInternet();
+        shadowOfActiveNetworkInfo.setConnectionStatus(NetworkInfo.State.DISCONNECTED);
+        assertFalse(Utils.isInternetAvailable(context));
+    }
+
+    @Test
+    public void checkIsInternetAvailableTrue_InternetOn() throws Exception{
+        this.setUpContextInternet();
+        shadowOfActiveNetworkInfo.setConnectionStatus(NetworkInfo.State.CONNECTED);
+        assertTrue(Utils.isInternetAvailable(context));
+    }
+
+    private void setUpContextInternet(){
+        context = getApplicationContext();
+        connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        shadowOfActiveNetworkInfo = shadowOf(connectivityManager.getActiveNetworkInfo());
     }
 }

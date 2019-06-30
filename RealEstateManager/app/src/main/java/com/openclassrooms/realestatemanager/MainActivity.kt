9923ab1e8f9352073b8973cf5dfed98b06c06e38
GalouM <galou.minisini@gmail.com>
2019-06-30 12:36:12 -0700

    tablayout and viewpager

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
@@ -6,14 +6,21 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TableLayout
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.viewpager.widget.ViewPager
 import butterknife.BindView
 import butterknife.ButterKnife
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
     @BindView(R.id.main_activity_toolbar) lateinit var toolbar: Toolbar
+    @BindView(R.id.main_activity_tablayout_viewpager) lateinit var viewPager: ViewPager
+    @BindView(R.id.main_activity_tablayout) lateinit var tabLayout: TabLayout
+
+    private val listDrawableIconTab = listOf(R.drawable.list_icon, R.drawable.map_icon)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +28,7 @@ class MainActivity : AppCompatActivity() {
         ButterKnife.bind(this)
 
         configureToolbar()
-    }
-
-    private fun configureToolbar(){
-        setSupportActionBar(toolbar)
+        configureViewPagerAndToolbar()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -40,6 +44,24 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun configureToolbar(){
+        setSupportActionBar(toolbar)
+    }
+
+    private fun configureViewPagerAndToolbar(){
+        fun setupIconTabLayout(){
+            tabLayout.getTabAt(0)?.setIcon(listDrawableIconTab[0])
+            tabLayout.getTabAt(1)?.setIcon(listDrawableIconTab[1])
+        }
+
+        viewPager.adapter = PageAdapterMainActivity(supportFragmentManager)
+        tabLayout.setupWithViewPager(viewPager)
+        tabLayout.tabMode = TabLayout.MODE_FIXED
+        setupIconTabLayout()
+
+
     }
 }
 
---
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/PageAdapterMainActivity.kt
@@ -0,0 +1,24 @@
+package com.openclassrooms.realestatemanager
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+/**
+ * Created by galou on 2019-06-30
+ */
+
+class PageAdapterMainActivity(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    override fun getItem(position: Int): Fragment {
+        return when(position){
+            0 -> ListPropertyView()
+            1 -> MapPropertyView()
+            else -> throw Exception ("No page found")
+        }
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+}

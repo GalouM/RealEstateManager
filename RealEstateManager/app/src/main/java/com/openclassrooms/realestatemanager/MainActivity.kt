266a9f518b818fe54fadadea49fddaa7694c2104
GalouM <galou.minisini@gmail.com>
2019-06-30 16:10:22 -0700

    custome extended fab

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.openclassrooms.realestatemanager
 
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,18 +8,29 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TableLayout
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.viewpager.widget.ViewPager
 import butterknife.BindView
 import butterknife.ButterKnife
 import com.google.android.material.tabs.TabLayout
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionButton
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionHelper
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionLayout
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RFACLabelItem
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RapidFloatingActionContentLabelList
+import com.wangjie.rapidfloatingactionbutton.util.RFABTextUtil
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), RapidFloatingActionContentLabelList.OnRapidFloatingActionContentLabelListListener<RFACLabelItem<Int>> {
 
     @BindView(R.id.main_activity_toolbar) lateinit var toolbar: Toolbar
     @BindView(R.id.main_activity_tablayout_viewpager) lateinit var viewPager: ViewPager
     @BindView(R.id.main_activity_tablayout) lateinit var tabLayout: TabLayout
+    @BindView(R.id.activity_main_rfal) lateinit var rfaLayout: RapidFloatingActionLayout
+    @BindView(R.id.activity_main_rfab) lateinit var rfaButton: RapidFloatingActionButton
+    private lateinit var rfabHelper: RapidFloatingActionHelper
 
     private val listDrawableIconTab = listOf(R.drawable.list_icon, R.drawable.map_icon)
 
@@ -29,6 +41,7 @@ class MainActivity : AppCompatActivity() {
 
         configureToolbar()
         configureViewPagerAndToolbar()
+        configureRapidFloatingActionButton()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -60,8 +73,48 @@ class MainActivity : AppCompatActivity() {
         tabLayout.setupWithViewPager(viewPager)
         tabLayout.tabMode = TabLayout.MODE_FIXED
         setupIconTabLayout()
+    }
 
+    private fun configureRapidFloatingActionButton() {
+        val rfaContent = RapidFloatingActionContentLabelList(applicationContext)
+        rfaContent.setOnRapidFloatingActionContentLabelListListener(this)
+        val items = mutableListOf<RFACLabelItem<Int>>()
+        items.add(RFACLabelItem<Int>()
+                .setLabel("Add Property")
+                .setResId(R.drawable.home_icon)
+                .setIconNormalColor(ContextCompat.getColor(applicationContext, R.color.colorTextPrimary))
+                .setIconPressedColor(ContextCompat.getColor(applicationContext, R.color.colorWhite))
+                .setWrapper(0)
+        )
+        items.add(RFACLabelItem<Int>()
 
+                .setLabel("Add agent")
+                .setResId(R.drawable.person_icon)
+                .setIconNormalColor(ContextCompat.getColor(applicationContext, R.color.colorTextPrimary))
+                .setIconPressedColor(ContextCompat.getColor(applicationContext, R.color.colorWhite))
+                .setWrapper(1)
+        )
+        rfaContent
+                .setItems(items as List<RFACLabelItem<Any>>?)
+                .setIconShadowRadius(RFABTextUtil.dip2px(applicationContext, 3F))
+                .setIconShadowColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
+                .setIconShadowDy(RFABTextUtil.dip2px(applicationContext, 3F))
+        rfabHelper = RapidFloatingActionHelper(
+                applicationContext,
+                rfaLayout,
+                rfaButton,
+                rfaContent
+        ).build()
+    }
+
+    override fun onRFACItemIconClick(position: Int, item: RFACLabelItem<RFACLabelItem<Int>>?) {
+        Toast.makeText(applicationContext, "clicked label: " + position, Toast.LENGTH_SHORT).show();
+        rfabHelper.toggleContent();
+    }
+
+    override fun onRFACItemLabelClick(position: Int, item: RFACLabelItem<RFACLabelItem<Int>>?) {
+        Toast.makeText(applicationContext, "clicked label: " + position, Toast.LENGTH_SHORT).show();
+        rfabHelper.toggleContent();
     }
 }
 
